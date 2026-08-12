4bcf9cb49332458fc237c7702b8329d5d51bac02
Zacharie PHILEMOND <zachariephilemond201@gmail.com>
2026-08-13 00:09:49 +0200

    Restucturation de du fichier

--- Connexion.php
+++ Connexion.php
@@ -1,52 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<style>
-    input{
-    padding: 16px;
-  border: 1px solid gray;
-  border-radius: 10px;
-  font-size: 16px;
-  width: 300px;
-    }
-    .cont{
-        display: flex;
-        flex-direction: column;
-        gap: 20px;
-        align-items: center;
-        margin-top: 220px;
-    }
-
-    .btn{
-        display: inline-block;
-    padding: 10px 20px;
-    background-color: grey;
-    color: white;
-    text-decoration: none;
-    border-radius: 5px;
-    font-weight: bold;
-
-    }
-
-</style>
-
-
-
-<body>
-    <form action="" method="POST" class="cont">
-    <input type="text" name ="utilisateur" placeholder="Nom d'utilisateur">
-    <input type="password" name="mot_de_passe" placeholder="Mot de passe">
-    <input type="submit" value="entrer">
-
-    <a href="inscription.php" class="btn">Créer un profil</a>
-    </form>
-
-   <?php
+<?php
 session_start();
 
 require_once __DIR__ . "/database.php";
@@ -57,7 +9,9 @@ if (isset($_POST["utilisateur"]) && isset($_POST["mot_de_passe"])) {
     $mot_de_passe = $_POST["mot_de_passe"];
 
     $sql = "SELECT * FROM utilisateur WHERE nom_utilisateur = :nom_utilisateur";
+
     $stmt = $pdo->prepare($sql);
+
     $stmt->execute([
         ":nom_utilisateur" => $nom_utilisateur
     ]);
@@ -73,8 +27,32 @@ if (isset($_POST["utilisateur"]) && isset($_POST["mot_de_passe"])) {
         exit();
 
     } else {
-        echo "Nom d'utilisateur ou mot de passe incorrect.";
+        $erreur = "Nom d'utilisateur ou mot de passe incorrect.";
     }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+<form action="" method="POST" class="cont">
+    <input type="text" name="utilisateur" placeholder="Nom d'utilisateur">
+    <input type="password" name="mot_de_passe" placeholder="Mot de passe">
+    <input type="submit" value="entrer">
+
+    <a href="inscription.php" class="btn">Créer un profil</a>
+</form>
+
+<?php
+if (isset($erreur)) {
+    echo $erreur;
 }
 ?>
 
