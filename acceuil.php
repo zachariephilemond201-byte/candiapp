154074679f17b9762304289f9e4f9ae3729384f0
Zacharie PHILEMOND <zachariephilemond201@gmail.com>
2026-08-13 13:41:26 +0200

    Correction de la Structure du fichier

--- acceuil.php
+++ acceuil.php
@@ -1,3 +1,21 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["id_utilisateur"])) {
+    header("Location: ./Connexion.php");
+    exit();
+}
+
+if (isset($_POST["deconnexion"])) {
+    session_unset();
+    session_destroy();
+
+    header("Location: ./Connexion.php");
+    exit();
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -36,21 +54,5 @@
 
 
 <body>
-    <h1>Bienvenue sur notre application, <?php session_start(); echo $_SESSION['nom_utilisateur']; ?></h1>
-    <div class="cont" method="post">
-        <a href="Ajout_entre.php" class="btn">Ajouter une entreprise</a>
-        <a href="liste_entre.php" class="btn">Voir vos entreprises</a>
-        <form method="post" style="display: inline;">
-            <input type="submit" class="btn" name="deconnexion" value="Se déconnecter">
-        </form>
-    </div>
-
-    <?php
-    if(isset($_POST["deconnexion"])) {
-        session_destroy();
-        header("Location: ./Connexion.php");
-        exit();
-    }
-    ?>
 </body>
 </html>
